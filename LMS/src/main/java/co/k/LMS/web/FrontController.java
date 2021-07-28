70fcb8d119fd63ca778ca881985e8421aeff2b01
foreveryoun910 <foreveryoun910@gmail.com>
2021-07-28 23:36:42 +0900

    ing

---
+++ LMS/src/main/java/co/k/LMS/web/FrontController.java
@@ -0,0 +1,48 @@
+package co.k.LMS.web;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.k.LMS.command.Command;
+import co.k.LMS.command.Main;
+
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private HashMap<String, Command> map = new HashMap<String, Command>();
+       
+    public FrontController() {
+        super();
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+		// Command 정의
+		map.put("/main.do", new Main());
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO 분석, 페이지 넘기기
+		request.setCharacterEncoding("utf-8");
+		String uri = request.getRequestURI();
+		String context = request.getContextPath();
+		String path = uri.substring(context.length());
+		Command command = map.get(path);
+		
+		String viewPage = command.execute(request, response);
+		if(!viewPage.endsWith(".do")) {
+			viewPage = "WEB-INF/views/" + viewPage + ".jsp";
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+	}
+
+}
